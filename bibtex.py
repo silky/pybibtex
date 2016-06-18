1d9226e7b34bf771e583668b24b53bf8ec804446
silky <noonsilk+-noonsilk@gmail.com>
2016-06-18 18:09:30 +1000

    emit filepaths

--- bibtex.py
+++ bibtex.py
@@ -1,16 +1,20 @@
 '''Utilities for formatting data to BibTeX format'''
 
-import re
+import re, codecs, unicodedata
 
 def format(data, reftype="article"):
     '''Formats a dict into a BibTeX entry'''
-    entry = "@{0}{{\n".format(reftype)
+    entry = "@{0}{{,\n".format(reftype)
     for key, val in data.iteritems():
         entry += "\t{0} = {{{1}}},\n".format(key, val)
-    entry += "}}"
-    return entry.encode('utf8')
+    entry += "}"
+    # return unicodedata.normalize('NFKD', entry).encode('utf-8', 'ignore')
+    return entry# .encode('utf8')
 
 def wrap(string):
-    '''Wrap all capital letters in curly braces to preserve their
-    capitalisation when output in LaTeX'''
-    return re.sub(r'([A-Z]+)', r'{\1}', string)
+    # Changed this because it's completely not needed for mh.
+    return string
+
+#     '''Wrap all capital letters in curly braces to preserve their
+#     capitalisation when output in LaTeX'''
+#     return re.sub(r'([A-Z]+)', r'{\1}', string)
